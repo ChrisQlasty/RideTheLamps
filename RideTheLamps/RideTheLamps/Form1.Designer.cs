26a42be6fdf470bbb07745947c1f7f46bd01ce97
Krzysztof Czuszynski <qlasty@hotmail.com>
2018-09-08 21:37:55 +0200

    Visual Studio Project files

---
+++ RideTheLamps/RideTheLamps/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace RideTheLamps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Ldataframe = new System.Windows.Forms.Label();
+            this.Bstart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RBsm2 = new System.Windows.Forms.RadioButton();
+            this.RBsm1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RBy = new System.Windows.Forms.RadioButton();
+            this.RBx = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Bblink = new System.Windows.Forms.Button();
+            this.Breset1 = new System.Windows.Forms.Button();
+            this.Breset2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 19200;
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Ldataframe
+            // 
+            this.Ldataframe.AutoSize = true;
+            this.Ldataframe.Location = new System.Drawing.Point(13, 55);
+            this.Ldataframe.Name = "Ldataframe";
+            this.Ldataframe.Size = new System.Drawing.Size(54, 13);
+            this.Ldataframe.TabIndex = 0;
+            this.Ldataframe.Text = "dataframe";
+            // 
+            // Bstart
+            // 
+            this.Bstart.Location = new System.Drawing.Point(12, 12);
+            this.Bstart.Name = "Bstart";
+            this.Bstart.Size = new System.Drawing.Size(75, 23);
+            this.Bstart.TabIndex = 1;
+            this.Bstart.Text = "Start";
+            this.Bstart.UseVisualStyleBackColor = true;
+            this.Bstart.Click += new System.EventHandler(this.Bstart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RBsm2);
+            this.groupBox1.Controls.Add(this.RBsm1);
+            this.groupBox1.Location = new System.Drawing.Point(163, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(107, 109);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selection Method";
+            // 
+            // RBsm2
+            // 
+            this.RBsm2.AutoSize = true;
+            this.RBsm2.Location = new System.Drawing.Point(7, 43);
+            this.RBsm2.Name = "RBsm2";
+            this.RBsm2.Size = new System.Drawing.Size(61, 17);
+            this.RBsm2.TabIndex = 1;
+            this.RBsm2.Text = "Sel M 2";
+            this.RBsm2.UseVisualStyleBackColor = true;
+            // 
+            // RBsm1
+            // 
+            this.RBsm1.AutoSize = true;
+            this.RBsm1.Checked = true;
+            this.RBsm1.Location = new System.Drawing.Point(7, 20);
+            this.RBsm1.Name = "RBsm1";
+            this.RBsm1.Size = new System.Drawing.Size(61, 17);
+            this.RBsm1.TabIndex = 0;
+            this.RBsm1.TabStop = true;
+            this.RBsm1.Text = "Sel M 1";
+            this.RBsm1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.RBy);
+            this.groupBox2.Controls.Add(this.RBx);
+            this.groupBox2.Location = new System.Drawing.Point(276, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(129, 109);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Brightness Controll";
+            // 
+            // RBy
+            // 
+            this.RBy.AutoSize = true;
+            this.RBy.Location = new System.Drawing.Point(6, 43);
+            this.RBy.Name = "RBy";
+            this.RBy.Size = new System.Drawing.Size(51, 17);
+            this.RBy.TabIndex = 1;
+            this.RBy.Text = "y axis";
+            this.RBy.UseVisualStyleBackColor = true;
+            // 
+            // RBx
+            // 
+            this.RBx.AutoSize = true;
+            this.RBx.Checked = true;
+            this.RBx.Location = new System.Drawing.Point(6, 20);
+            this.RBx.Name = "RBx";
+            this.RBx.Size = new System.Drawing.Size(51, 17);
+            this.RBx.TabIndex = 0;
+            this.RBx.TabStop = true;
+            this.RBx.Text = "x axis";
+            this.RBx.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Bblink
+            // 
+            this.Bblink.Location = new System.Drawing.Point(16, 127);
+            this.Bblink.Name = "Bblink";
+            this.Bblink.Size = new System.Drawing.Size(75, 23);
+            this.Bblink.TabIndex = 5;
+            this.Bblink.Text = "Blink";
+            this.Bblink.UseVisualStyleBackColor = true;
+            this.Bblink.Click += new System.EventHandler(this.Bblink_Click);
+            // 
+            // Breset1
+            // 
+            this.Breset1.Location = new System.Drawing.Point(163, 127);
+            this.Breset1.Name = "Breset1";
+            this.Breset1.Size = new System.Drawing.Size(75, 23);
+            this.Breset1.TabIndex = 7;
+            this.Breset1.Text = "Reset";
+            this.Breset1.UseVisualStyleBackColor = true;
+            this.Breset1.Click += new System.EventHandler(this.Breset1_Click);
+            // 
+            // Breset2
+            // 
+            this.Breset2.Location = new System.Drawing.Point(276, 127);
+            this.Breset2.Name = "Breset2";
+            this.Breset2.Size = new System.Drawing.Size(75, 23);
+            this.Breset2.TabIndex = 8;
+            this.Breset2.Text = "Reset";
+            this.Breset2.UseVisualStyleBackColor = true;
+            this.Breset2.Click += new System.EventHandler(this.Breset2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 171);
+            this.Controls.Add(this.Breset2);
+            this.Controls.Add(this.Breset1);
+            this.Controls.Add(this.Bblink);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Bstart);
+            this.Controls.Add(this.Ldataframe);
+            this.Name = "Form1";
+            this.Text = "Ride the Lamps";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label Ldataframe;
+        private System.Windows.Forms.Button Bstart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RBsm2;
+        private System.Windows.Forms.RadioButton RBsm1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton RBy;
+        private System.Windows.Forms.RadioButton RBx;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Bblink;
+        private System.Windows.Forms.Button Breset1;
+        private System.Windows.Forms.Button Breset2;
+    }
+}
+
---
+++ RideTheLamps/RideTheLamps/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+
+namespace RideTheLamps
+{
+    public partial class Form1 : Form
+    {
+        string line = "";        
+        delegate void LampSelNotifier();
+        LampSelNotifier MyNotifier;
+        
+        public ConcurrentQueue<char> serialDataQueue;
+
+        private static string WS_url = "http://192.168.0.100";
+        private static string usrKey = "cNj0Rz4LRnNgawBVYwU2JPhJZ3nawoz2p9oINbxx";
+        static Uri url = null;
+        static bool status = false;
+        int Xval=50, Yval=50;
+        static bool canEnter = true;
+        bool nomore = true;
+        static string currentLamp = "/lights/1/state";
+        static int Lamp = 1;                
+
+        static int Lamp1Color = 47000;
+        static int Lamp2Color = 25500;
+        static int Lamp3Color = 5500;
+        static int LampAlertColor = 65000;
+
+        static float bri_range = 2.5f;
+
+        private System.Timers.Timer timer, timer2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialDataQueue = new ConcurrentQueue<char>();
+
+            timer = new System.Timers.Timer();            
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Interval = 250;
+
+            timer2 = new System.Timers.Timer();            
+            timer2.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed2);
+            timer2.Interval = 300;            
+
+            MyNotifier = notifyLampSelected;
+        }
+
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {    
+            SerialPort sp = sender as SerialPort;
+            int bytesAvailable = sp.BytesToRead;
+
+            char[] recBuf = new char[bytesAvailable];
+            try
+            {
+                sp.Read(recBuf, 0, bytesAvailable);
+                for (int index = 0; index < bytesAvailable; index++)
+                    serialDataQueue.Enqueue(recBuf[index]);
+            }
+            catch (Exception eee) { }
+
+            readSerialDequeue();
+        }
+
+        void readSerialDequeue()
+        {
+            char ch;
+            try
+            {
+                while (serialDataQueue.TryDequeue(out ch))
+                {             
+                    if (ch.Equals('\n'))
+                    {
+                        if(canEnter)
+                        {
+                            ReadDataFrame();                            
+                        }
+                        line = "";
+                    }
+                    else
+                        line += ch;
+                }
+            }
+            catch (Exception e) { }
+        }
+
+
+        public void ReadDataFrame()
+        {
+            Ldataframe.Invoke(new Action(delegate()
+            {
+                Ldataframe.Text = line;
+            }));            
+            
+            //---REMOVE DATAFRAME FORMATTING CHARACTERS-----
+            line = line.Replace('!', ' ');
+            line = line.Replace('#',' ');
+            string[] metadata = line.Split(new char[]{' '},5);
+            metadata[4] = metadata[4].TrimEnd(new Char[] { ' ' });
+           
+            Int32.TryParse(metadata[1].TrimStart(new Char[] { '0' }), out Xval);
+            Int32.TryParse(metadata[2].TrimStart(new Char[] { '0' }), out Yval);
+
+            nomore = true;
+
+            //-TURNING LIGHT SOURCE ON/OFF USING HAND SWIPES--
+            //----BACKWARD-> TURNS ON---FORWARD-> TURNS OFF---
+            //------------------------------------------------          
+            if (metadata[4].Length > 3)
+            {
+                if (metadata[4].Substring(3, 1).Contains("B"))
+                {
+                    SendCMD("on", "true");                    
+                    nomore = false;
+                }
+                else if (metadata[4].Substring(3, 1).Contains("F"))
+                {
+                    SendCMD("on", "false");                    
+                    nomore = false;
+                }
+            }                        
+
+            //------ACTIVE LIGHT SOURCE SELECTION METHODS-----
+            //------------------------------------------------
+            if (RBsm1.Checked && nomore)
+            {
+                if (metadata[4].Equals("4FJ") && Xval > 60 && canEnter)
+                {                    
+                    currentLamp = "/lights/2/state";
+                    Lamp = 2;                    
+                    MyNotifier();                                        
+                    nomore = false;
+                }
+                else if (metadata[4].Equals("4FJ") && Xval < 40 && canEnter)
+                {                    
+                    currentLamp = "/lights/1/state";
+                    Lamp = 1;                    
+                    MyNotifier();
+                    nomore = false;
+                }
+                else if (metadata[4].Equals("2FS") && canEnter)
+                {                    
+                    //currentLamp = "/groups/0/action";
+                    Lamp = 4;                    
+                    MyNotifier();
+                    nomore = false;
+                }
+            }
+            else if (RBsm2.Checked && nomore)
+            {
+                if (metadata[4].Equals("1FS") && canEnter)
+                {                    
+                    currentLamp = "/lights/1/state";
+                    Lamp = 1;                    
+                    MyNotifier();
+                    nomore = false;
+                }
+                else if (metadata[4].Equals("2FS") && canEnter)
+                {                   
+                    currentLamp = "/lights/2/state";
+                    Lamp = 2;                    
+                    MyNotifier();
+                    nomore = false;
+                }
+                else if (metadata[4].Equals("2FJ") && canEnter)
+                {                    
+                    //currentLamp = "/groups/0/action";
+                    Lamp = 4;                    
+                    MyNotifier();
+                    nomore = false;
+                }
+            }
+           
+            canEnter = nomore;
+
+            if (canEnter == false)
+                timer2.Start();
+
+            //------------------------------------------------
+            int Lthres = 4;
+            //--BRIGHTNESS ADJUSTMENT BY CONTIUNUOUS GESTURES-
+            if (metadata[3].Equals("1FS") && nomore && RBsm1.Checked)
+            {
+                if (counter > Lthres)
+                {
+                    if (RBx.Checked)
+                        SendCMD("bri", ((int)(bri_range * (double)Xval)).ToString());                     
+                    else
+                        SendCMD("bri", ((int)(bri_range * (double)Yval)).ToString());                   
+                    counter = 0;
+                }
+
+                counter++;
+            }
+
+            if (metadata[3].Equals("4FJ") && nomore && RBsm2.Checked)
+            {
+                if (counter > Lthres)
+                {
+                    if (RBx.Checked)
+                        SendCMD("bri", ((int)(bri_range * (double)Xval)).ToString());
+                    else
+                        SendCMD("bri", ((int)(bri_range * (double)Yval)).ToString());  
+                    counter = 0;
+                }
+
+                counter++;
+            }
+
+        }
+        int counter = 0;
+        public void notifyLampSelected()
+        {            
+            timer.Start();            
+        }
+
+        
+        async static void setLight(string myQuerry)
+        {
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage();            
+            var content = new StringContent(myQuerry, Encoding.UTF8, "application/json");
+
+            try
+            {
+                url = new Uri(WS_url + "/api/" + usrKey + currentLamp);
+                HttpResponseMessage returnStatement = await client.PutAsync(url, content);
+                string res = await returnStatement.Content.ReadAsStringAsync();
+                Console.WriteLine("Return Statment:" + res);                
+            }
+            catch (Exception e) { }            
+        }
+
+        private void Bstart_Click(object sender, EventArgs e)
+        {
+            initialState();
+
+            serialPort1.ReceivedBytesThreshold = 10;
+
+            if (!serialPort1.IsOpen)
+                serialPort1.Open(); 
+        }
+        int nLSc = 0;
+        int mcnt = 0;
+        int mean_lvl = 60;
+        int max_lvl = 150;
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {            
+            if (mcnt == 0)
+            {
+                if (Lamp == 1)
+                    SendCMD("on", "true", "bri", (mean_lvl).ToString(), "hue", (Lamp1Color).ToString(), "sat", "155");
+                else
+                    SendCMD("on", "true", "bri", (mean_lvl).ToString(), "hue", (Lamp2Color).ToString(), "sat", "155");
+            }
+            else if (mcnt == 1)
+            {
+                SendCMD("bri", (max_lvl).ToString(), "hue", (LampAlertColor).ToString(), "sat", "254");
+            }
+            else if (mcnt == 2)
+            {
+                if (Lamp == 1)
+                    SendCMD("bri", (mean_lvl).ToString(), "hue", (Lamp1Color).ToString(), "sat", "155");
+                else
+                    SendCMD("bri", (mean_lvl).ToString(), "hue", (Lamp2Color).ToString(), "sat", "155");
+            }
+            else
+            {
+                mcnt = -1;
+                timer.Stop();                
+            }
+            mcnt++;
+
+        }
+
+        int nCNTR=0;
+        private void timer_Elapsed2(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (nCNTR == 0)
+            {
+
+            }else if (nCNTR == 1)
+            {
+                canEnter = true;
+                nCNTR = 0;
+                timer2.Stop();
+            }
+            nCNTR++;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("no {0}", mcnt);
+         
+            if (mcnt == 0)
+            {                
+                if(Lamp==1)                    
+                    SendCMD("on","true", "bri", (mean_lvl).ToString(), "hue", (Lamp1Color).ToString(), "sat", "155");
+                else
+                    SendCMD("on", "true", "bri", (mean_lvl).ToString(), "hue", (Lamp2Color).ToString(), "sat", "155");
+            }
+            else if (mcnt == 1)
+            {                                
+                    SendCMD("bri", (max_lvl).ToString(), "hue", (LampAlertColor).ToString(), "sat", "254");
+            }
+            else if (mcnt == 2)
+            {
+                if (Lamp == 1)
+                    SendCMD("bri", (mean_lvl).ToString(), "hue", (Lamp1Color).ToString(), "sat", "155");
+                else
+                    SendCMD("bri", (mean_lvl).ToString(), "hue", (Lamp2Color).ToString(), "sat", "155");
+            }         
+            else
+            {
+                mcnt = -1;
+                timer1.Stop();
+                Console.WriteLine("no exit {0}", mcnt);
+            }
+            mcnt++;
+        }
+
+        private void Bblink_Click(object sender, EventArgs e)
+        {
+            MyNotifier();
+        }
+      
+        void initialState()
+        {         
+            currentLamp = "/lights/2/state";
+                SendCMD("on", "true", "hue", (Lamp2Color).ToString(), "bri", (100).ToString(), "sat", "195");
+                    System.Threading.Thread.Sleep(125);
+            currentLamp = "/lights/1/state";
+                SendCMD("on", "true", "hue", (Lamp1Color).ToString(), "bri", (100).ToString(), "sat", "195");
+        }
+
+        void SendCMD(params string[] pars)
+        {
+            String myJson = "{";            
+
+            int LEN = pars.Length;
+            for (int i = 0; i<LEN; i+=2)
+            {
+                myJson += "\"" + pars[i] + "\":" + pars[i+1];
+                if (i + 2 < LEN)
+                    myJson += ",";
+            }
+            myJson += "}";
+
+            if (Lamp == 4)
+            {
+                currentLamp = "/lights/1/state";
+                setLight(myJson);
+                    System.Threading.Thread.Sleep(125);
+                currentLamp = "/lights/2/state";
+                setLight(myJson);                
+            }
+            else
+                setLight(myJson);
+        }      
+
+        private void Breset1_Click(object sender, EventArgs e)
+        {             
+            currentLamp = "/lights/2/state";
+            SendCMD("hue", (10000).ToString(), "bri", (150).ToString(), "sat", "0"); ;        
+        }
+
+        private void Breset2_Click(object sender, EventArgs e)
+        {
+            currentLamp = "/lights/1/state";
+            SendCMD("hue", (10000).ToString(), "bri", (150).ToString(), "sat", "0"); 
+        }
+    }
+}
